d8ef02f5eb483b20fc206e3fff2eddcac86e0435
Jeremy Price <jprice.rhit@gmail.com>
2015-10-14 07:47:27 -0500

    added processing example

---
+++ examples/example_process_workers.py
@@ -0,0 +1,25 @@
+import multiprocessing
+import os
+
+def worker(q):
+    process_id = os.getpid()
+    while True:
+        item = q.get()
+        print "Worker %s:" % process_id, item
+        q.task_done()
+
+if __name__ == "__main__":
+    num_worker_processes = 5
+    num_work_items = 20
+    q = multiprocessing.JoinableQueue(num_work_items+1)
+    for i in range(num_worker_processes):
+        p = multiprocessing.Process(target=worker, args=[q])
+        p.daemon = True
+        p.start()
+
+    work_items = xrange(num_work_items)
+    for item in work_items:
+        q.put(item)
+
+    q.join()       # block until all tasks are done
+    print "All done"
