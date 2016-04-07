caeced1e049299e83975ff69d5dad65cc324fbf1
Jeremy Price <jprice.rhit@gmail.com>
2016-04-07 08:19:20 -0500

    added flask labs

---
+++ labs/lab_flask.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# *-* coding:utf-8 *-*
+
+"""
+
+:mod:`lab_flask` -- serving up REST
+=========================================
+
+LAB_FLASK Learning Objective: Learn to serve RESTful APIs using the Flask library
+::
+
+ a. Using Flask create a simple server that serves the following string for the root route ('/'):
+  "<h1>Welcome to my server</h1>"
+
+ b. Add a route for "/now" that returns the current date and time in string format.
+
+ c. Add a route that converts Fahrenheit to Centgrade and accepts the value to convert
+    in the url.  For instance, /fahrenheit/32.0 should return "0.0"
+
+ d. Add a route that converts Centigrade to Fahrenheit and accepts the value to convert
+    in the url.  For instance, /centigrade/0.0 should return "32.0"
+
+"""
---
+++ solutions/lab_flask.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2
+# *-* coding:utf-8 *-*
+
+"""
+
+:mod:`lab_flask` -- serving up REST
+=========================================
+
+LAB_FLASK Learning Objective: Learn to serve RESTful APIs using the Flask library
+::
+
+ a. Using Flask create a simple server that serves the following string for the root route ('/'):
+  "<h1>Welcome to my server</h1>"
+
+ b. Add a route for "/now" that returns the current date and time in string format.
+
+ c. Add a route that converts Fahrenheit to Centgrade and accepts the value to convert
+    in the url.  For instance, /fahrenheit/32.0 should return "0.0"
+
+ d. Add a route that converts Centigrade to Fahrenheit and accepts the value to convert
+    in the url.  For instance, /centigrade/0.0 should return "32.0"
+
+"""
+
+from flask import Flask
+import datetime
+
+
+def f_to_c(f):
+    return 5.0 / 9.0 * (f - 32.0)
+
+
+def c_to_f(c):
+    return (9.0 / 5.0 * c) + 32.0
+
+
+app = Flask(__name__)
+
+
+@app.route('/now')
+def now():
+    return datetime.datetime.now().ctime()
+
+
+@app.route('/')
+def hello():
+    return "<h1>Welcome to my server</h1>"
+
+
+@app.route('/fahrenheit/<int:f>')
+@app.route('/fahrenheit/<float:f>')
+def fahrenheit(f):
+    return str(f_to_c(f))
+
+
+@app.route('/centigrade/<int:c>')
+@app.route('/centigrade/<float:c>')
+def centigrade(c):
+    return str(c_to_f(c))
+
+
+if __name__ == '__main__':
+    app.run()
