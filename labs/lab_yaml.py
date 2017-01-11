0705421a60a1e028d8a01a4b2078c6a2d78db0a5
Jeremy Price <jprice.rhit@gmail.com>
2017-01-11 09:45:44 -0600

    added lab_yaml

---
+++ labs/lab_yaml.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# *-* coding:utf-8 *-*
+
+"""
+
+:mod:`lab_yaml` -- YAML Parsing
+=========================================
+
+LAB_YAML Learning Objective: Learn to parse a YAML file using the PyYAML library
+                             and use the information.
+::
+
+ a. Start with your code (or the solution code) from lab_multi.
+
+ b. Replace the command line config parsing with a YAML config parsing function.  Use the first
+    argument to the program as the config file to parse.
+
+ c. Create a YAML file that contains the same config items as the old command line.  Verify you
+    can configure the program by changing the config file options.
+
+"""
